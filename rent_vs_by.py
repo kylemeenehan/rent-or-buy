3d98b35e767249e8a0b699537df16e45f00fab01
Kyle Meenehan <kylemeenehan@gmail.com>
2018-12-09 09:02:21 +0200

    output results in dataframe

--- rent_vs_by.py
+++ rent_vs_by.py
@@ -11,7 +11,7 @@ investment_rate = 0.09
 
 # property purchase
 
-property_price = 1000000
+property_price = 1400000
 initiation_fee = 50000
 
 bond_interest_rate = 0.105
@@ -30,7 +30,7 @@ purchase_costs = {
 # property rental
 
 rental_costs = {
-    "rent": 6000,
+    "rent": 9000,
     "utilities": 1000,
     "wages": 2700
 }
@@ -54,56 +54,78 @@ def cost_of_scenario(cost_dict):
 print("==== Property purchase ===")
 
 total_loan_amount = property_price + initiation_fee
-bond_monthly_payment = np.pmt(bond_interest_rate / 12, bond_duration_in_years * 12, total_loan_amount)
 property_final_value = -np.fv(rate=property_annual_appreciation, nper=bond_duration_in_years, pmt=0, pv=property_price)
 
+bond_monthly_payment = np.pmt(bond_interest_rate / 12, bond_duration_in_years * 12, total_loan_amount)
 purchase_other_monthly_costs = reduce(operator.add, [x for x in purchase_costs.values()])
+purchase_total_monthly_costs = bond_monthly_payment - purchase_other_monthly_costs
 
-initial_monthly_investment_amount = allocated_monthly_funds \
+purchase_initial_monthly_investment_amount = allocated_monthly_funds \
     + bond_monthly_payment \
-    - reduce(operator.add, [x for x in purchase_costs.values()])
-investment_final_value = -np.fv(rate=investment_rate/12, nper=bond_duration_in_years*12, pmt=initial_monthly_investment_amount, pv=0)
+    - purchase_other_monthly_costs
+purchase_investment_final_value = -np.fv(rate=investment_rate/12, nper=bond_duration_in_years*12, pmt=purchase_initial_monthly_investment_amount, pv=0)
 
-purchase_nett_assets = round(investment_final_value + property_final_value, 2)
+purchase_nett_assets = round(purchase_investment_final_value + property_final_value, 2)
 
-output_purchase = f"""
-Total loan amount: {total_loan_amount}
-Monthly bond payment: {round(bond_monthly_payment, 2)}
-Monthly other costs: {purchase_other_monthly_costs}
-Total monthly: {round(bond_monthly_payment - purchase_other_monthly_costs, 2)}
-Property value once paid off: {round(property_final_value, 2)}
-
-Monthly investment amount: {round(initial_monthly_investment_amount, 2)}
-Final value of investment: {round(investment_final_value, 2)}
+print(f"""
+Total loan amount: {total_loan_amount:,}
+Monthly bond payment: {round(bond_monthly_payment, 2):,}
+Monthly other costs: {purchase_other_monthly_costs:,}
+Total monthly: {round(purchase_total_monthly_costs, 2):,}
+Property value once paid off: {round(property_final_value, 2):,}
+Monthly investment amount: {round(purchase_initial_monthly_investment_amount, 2):,}
+Final value of investment: {round(purchase_investment_final_value, 2)}
 
 Nett assets: {purchase_nett_assets:,}
-
-"""
-print(output_purchase)
+""")
 
 
 print("==== Property rental ===")
 
-initial_monthly_investment_amount = allocated_monthly_funds - reduce(operator.add, [x for x in rental_costs.values()])
-investment_final_value = -np.fv(rate=investment_rate/12, nper=bond_duration_in_years*12, pmt=initial_monthly_investment_amount, pv=0)
+rent_initial_monthly_investment_amount = allocated_monthly_funds - reduce(operator.add, [x for x in rental_costs.values()])
+rent_investment_final_value = -np.fv(rate=investment_rate/12, nper=bond_duration_in_years*12, pmt=rent_initial_monthly_investment_amount, pv=0)
 
-rent_net_assets = investment_final_value
+rent_net_assets = rent_investment_final_value
+rent_total_monthly_costs = reduce(operator.add, [x for x in rental_costs.values()])
+total_rent_costs = cost_of_scenario(rental_costs);
 
-print(f"Initial monthly rent: {rental_costs['rent']}")
+print(f"""
+Initial monthly rent: {rental_costs['rent']:,}
+Initial monthly total cost: {rent_total_monthly_costs:,}
+Total of all costs in rental scenario: {round(total_rent_costs, 2):,}
+Initial monthly investment:{rent_initial_monthly_investment_amount:,}
 
-total_rent_cost = cost_of_scenario(rental_costs);
-print(f"Total of all costs in rental scenario: {round(total_rent_cost, 2)}")
+Final value of investment: {round(rent_investment_final_value):,}
+""")
 
-output_rent = f"""
-Initial monthly investment:{initial_monthly_investment_amount}
-Final value of investment: {round(investment_final_value):,}
-"""
+d = {
+    "purchase": [
+        f"{round(purchase_total_monthly_costs):,}",
+        f"{round(purchase_initial_monthly_investment_amount):,}",
+        f"{round(property_final_value):,}",
+        f"{round(purchase_investment_final_value):,}",
+        f"{round(purchase_nett_assets):,}",
+    ],
+    "rent": [
+        f"{round(-rent_total_monthly_costs):,}",
+        f"{round(rent_initial_monthly_investment_amount):,}",
+        f"N/A",
+        f"{round(rent_investment_final_value):,}",
+        f"{round(rent_investment_final_value):,}",
+    ]
+}
 
-print(output_rent)
+results = pd.DataFrame(d, index=["Initial monthly payments",
+                       "Initial monthly investement",
+                       "Property final value",
+                       "Investment final value",
+                       "Final nett assets"])
+
+print(results)
 
 scenario_difference = round(abs(rent_net_assets - purchase_nett_assets))
 
-print("Rent or purchase?\n")
+print("\nRent or purchase?\n")
 if rent_net_assets > purchase_nett_assets:
     print( "RENT")
 elif purchase_nett_assets > rent_net_assets:
